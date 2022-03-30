fb12ba75746c477f7a3c5ec656bc9cb99c12b9d2
Franco Ruben Melazo <92115210+mela94@users.noreply.github.com>
2022-03-30 20:51:36 -0300

    Add files via upload

---
+++ practica1/app.js
@@ -0,0 +1,22 @@
+
+function add7(numero) {
+    return numero + 7
+};
+
+console.log(add7(10));
+
+
+function multiply(numero) {
+    return numero * numero;
+}
+
+console.log(multiply(10));
+
+function capitalize(valor) {
+    return valor.charAt(" ");
+}
+console.log(capitalize("hola soy paula"));
+
+
+let cadena = "abcd"
+console.log(cadena.substr(-1))
---
+++ practica2/app.js
@@ -0,0 +1,14 @@
+let respuesta = parseInt(prompt("Ingrese numero porfavor:"));
+
+for (let i = 1; i <= respuesta; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if(i % 5 === 0) {
+        console.log("Buzz")
+    } else{
+        console.log(i);
+    }
+}
